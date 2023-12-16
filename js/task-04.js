5e9e7a80392c9c1d871fdca4f32d7feca63ea7ca
SvKost <skost0634610557@gmail.com>
2023-12-16 17:37:09 +0200

    Upd task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,17 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const elems = event.target.elements;
+  const data = {
+    email: elems.email.value.trim(),
+    password: elems.password.value.trim(),
+  };
+
+  if (elems.email.value === '' || elems.password.value === '') {
+    alert('All form fields must be filled in');
+  } else {
+    console.log(data);
+  }
+  form.reset();
+});

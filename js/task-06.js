fc07c986d2d865ed86a104684b6aa72dbd08dfcb
SvKost <skost0634610557@gmail.com>
2023-12-16 19:31:16 +0200

    Upd task-06

--- js/task-06.js
+++ js/task-06.js
@@ -3,3 +3,38 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesDiv = document.querySelector('#boxes');
+
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const amount = input.value;
+  if (amount >= 1 && amount <= 100) {
+    destroyBoxes();
+
+    const boxesArray = Array.from({ length: amount });
+    let size = 30;
+
+    boxesArray.forEach(() => {
+      const box = document.createElement('div');
+
+      box.style.width = `${size}px`;
+      box.style.height = `${size}px`;
+      box.style.backgroundColor = getRandomHexColor();
+
+      boxesDiv.appendChild(box);
+      size += 10;
+    });
+
+    input.value = '';
+  }
+}
+
+function destroyBoxes() {
+  boxesDiv.innerHTML = '';
+}
